1e020a193e8a4b3d46742f8e9e78555f4bc01a62
jcraddock2 <jcraddock2@yahoo.com>
2026-03-25 15:54:54 -0400

    Create core.ts

---
+++ app/lib/core.ts
@@ -0,0 +1,7 @@
+// app/lib/proverbs/core.ts
+
+import { createProverb, type ProverbEntry } from "./shared";
+
+export const CORE_PROVERBS: ProverbEntry[] = [
+  // paste your CURRENT proverb entries here
+];
