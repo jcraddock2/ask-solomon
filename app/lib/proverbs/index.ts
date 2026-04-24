7f056dbead9d036be562a02955131e9c6e37711e
jcraddock2 <jcraddock2@yahoo.com>
2026-04-24 13:20:11 -0400

    Update index.ts

--- app/lib/proverbs/index.ts
+++ app/lib/proverbs/index.ts
@@ -1,11 +1,15 @@
 // app/lib/proverbs/index.ts
 
-import { CORE_PROVERBS } from "./core";
 import { PROVERBS_1_9 } from "./proverbs_1_9";
-import type { ProverbEntry } from "./shared";
+import { PROVERBS_10_15 } from "./proverbs_10_15";
+import { PROVERBS_16_20 } from "./proverbs_16_20";
+import { PROVERBS_21_25 } from "./proverbs_21_25";
+import { PROVERBS_26_31 } from "./proverbs_26_31";
 
-export const PROVERBS: ProverbEntry[] = [
-  ...CORE_PROVERBS,
+export const PROVERBS = [
   ...PROVERBS_1_9,
+  ...PROVERBS_10_15,
+  ...PROVERBS_16_20,
+  ...PROVERBS_21_25,
+  ...PROVERBS_26_31,
 ];
-
