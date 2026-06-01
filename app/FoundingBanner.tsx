75da86756b614a6f9dcf761e6baf03adf56ef3d9
jcraddock2 <jcraddock2@yahoo.com>
2026-06-01 14:26:23 -0400

    Reset founding member countdown to 9 days (June 10, 2026)

--- app/FoundingBanner.tsx
+++ app/FoundingBanner.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 
-const END = new Date("2026-06-04T23:59:59Z").getTime();
+const END = new Date("2026-06-10T23:59:59Z").getTime();
 const P = (n: number) => String(n).padStart(2, "0");
 
 function checkPro(): boolean {
